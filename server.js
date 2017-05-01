e7dd816ec26f905fa399678612a93006805a2b83
Anthony Myhre <anthony.myhre@utexas.edu>
2017-04-30 20:52:10 -0500

    Updated email functionality Added an email that responds to the sender

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/portfolio', function (req, res) {
 	res.sendfile('portfolio.html');
 });
 
-app.get('/contact', function(req, res) {
+app.get('/contact', function (req, res) {
 	res.sendfile('contact.html');
 });
 
@@ -41,10 +41,35 @@ app.post('/sendEmail', function (req, res) {
 		]
 	})
 		.then(function (data) {
+			client.transmissions.send({
+				content: {
+					from: 'admin@anthonymyhre.com',
+					subject: 'Message from Anthony Myhre',
+					html: '<html><body>Hello ' + req.body.name +',<br /><br />Thank you for contacting me from my portfolio. I\'ve received your email and will get back to you as soon as possible.<br /><br />' + 'Best,<br />Anthony Myhre<br />Full Stack Developer<br />' +
+						'<a href="http://www.anthonymyhre.com">Portfolio</a><br />' +
+						'<a href="https://www.linkedin.com/in/anthony-myhre/">LinkedIn</a><br />' +
+						'<a href="https://github.com/amyhre16">GitHub</a><br />' +
+					'</body></html>'
+				},
+				recipients: [
+					{ address: req.body.email }
+				]
+			})
+				.then(function (data) {
+
+					console.log('Woohoo! You just sent your first mailing!');
+					console.log(data);
+
+					res.json("The message was sent!");
+				})
+				.catch(function (err) {
+					console.log('Whoops! Something went wrong');
+					console.log(err);
+				});
+
 			console.log('Woohoo! You just sent your first mailing!');
 			console.log(data);
 
-			res.json("The message was sent!");
 		})
 		.catch(function (err) {
 			console.log('Whoops! Something went wrong');
